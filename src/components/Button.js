503685ac02fbb3605341b4c8d8f4341d0383e75a
Your Name <you@example.com>
2022-11-04 21:30:50 +0300

    plus-minus

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,26 +5,62 @@ export class Button extends Component {
     constructor() {
         super();
         this.state = {
-            isActive: false,
-            label: "Click",
+           count: 1
         };
     }
 
+
+
     registerEvents() {
-        this.addEventListener('click', () => {
-            this.setState((state) => {
-                return {
-                    ...state,
-                    isActive: !this.isActive,
-                }
-            })
+        // this.addEventListener('click', () => {
+        //     this.setState((state) => {
+        //         return {
+        //             ...state,
+        //             isActive: !this.isActive,
+        //         }
+        //     })
+        // })
+
+        this.addEventListener('click', (event) => {
+            if(event.target.closest('.plus')) {
+                this.setState((state) => {
+                    return {
+                        ...state,
+                        count: state.count < 10 ?  state.count +=1 : state.count
+                    }
+                })
+            }
+            if(event.target.closest('.minus')) {
+                this.setState((state) => {
+                    return {
+                        ...state,
+                        count: state.count > 0 ?  state.count -=1 : state.count
+                    }
+                })
+            }
+            
+           
         })
+
+
+
+
     }
+
+    
+
+    // render() {
+    //     return `
+    //         <button class ='${this.state.isActive ? "active" : ""}'>Click</button>
+    //     `
+    // }
 
     render() {
         return `
-            <button class ='${this.state.isActive ? "active" : ""}'>Click</button>
-        `
+            <button class="plus">+</button>
+            <span>${this.state.count}</span>
+            <button class="minus">-</button>
+        `;
     }
 
 }
