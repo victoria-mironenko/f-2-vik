6fcce46bd0906f4e30fc5ba7ca6aab59eff5ba69
Vika Mironenko <mironenko-vika-2010@mail.ru>
2022-11-07 21:11:39 +0300

    fixed bugs

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,59 +1,44 @@
 import './button.scss'
-import { Component} from "../core";
+import { Component } from "../core";
 
 export class Button extends Component {
     constructor() {
         super();
         this.state = {
-           count: 1
+            count: 1
         };
     }
 
-
-
-    registerEvents() {
-        // this.addEventListener('click', () => {
-        //     this.setState((state) => {
-        //         return {
-        //             ...state,
-        //             isActive: !this.isActive,
-        //         }
-        //     })
-        // })
-
-        this.addEventListener('click', (event) => {
-            if(event.target.closest('.plus')) {
-                this.setState((state) => {
-                    return {
-                        ...state,
-                        count: state.count < 10 ?  state.count +=1 : state.count
-                    }
-                })
+    plus() {
+        this.setState((state) => {
+            return {
+                ...state,
+                count: state.count < 10 ? state.count += 1 : state.count,
             }
-            if(event.target.closest('.minus')) {
-                this.setState((state) => {
-                    return {
-                        ...state,
-                        count: state.count > 0 ?  state.count -=1 : state.count
-                    }
-                })
-            }
-            
-           
-        })
-
-
-
-
+        });
     }
 
-    
+    minus() {
+        this.setState((state) => {
+            return {
+                ...state,
+                count: state.count > 0 ? state.count -= 1 : state.count,
+            }
+        });
+    }
 
-    // render() {
-    //     return `
-    //         <button class ='${this.state.isActive ? "active" : ""}'>Click</button>
-    //     `
-    // }
+    registerEvents() {
+        this.addEventListener('click', (evt) => {
+            if (evt.target.closest('.plus')) {
+                this.plus();
+            }
+            if (evt.target.closest('.minus')) {
+                this.minus();
+            }
+        })
+    }
+
+
 
     render() {
         return `
